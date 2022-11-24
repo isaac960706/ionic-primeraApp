1e66b3f11cecd7985371b06be0cfe89b812ea038
Isaac Martinez <iscmtzbvo@gmail.com>
2022-11-23 18:09:34 -0600

    fin de la session 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
 
+  ngOnInit() {
+    let num = 100;
+    // debugger;
+
+    num = 20;
+
+    num = num * 5;
+
+    num = 1;
+
+    console.log("num: ", num);
+
+
+  }
 }
